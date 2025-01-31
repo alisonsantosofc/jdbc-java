648b4658d9f8a7ad9058495a7b45321c61c1eb1e
alisonsantosofc <alisonsantoso22@gmail.com>
2025-01-31 09:02:34 -0300

    feat: create seller class

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,10 +1,22 @@
 package com.app;
 
+import java.util.Date;
+
 import com.app.models.entities.Department;
+import com.app.models.entities.Seller;
 
 public class App {
   public static void main(String[] args) {
     Department department = new Department(1, "Books");
-    System.out.println(department);
+    Seller seller = new Seller(
+      1, 
+      "John Doe", 
+      "john@email.com", 
+      new Date(), 
+      3000.0, 
+      department
+    );
+
+    System.out.println(seller);
   }
 }
